0d7ea95b8f8a0efb6ba6eb01e89c49b566c38d29
Adi <g.adicandra0@gmail.com>
2025-11-27 00:38:19 +0700

    Vercel Fixing

--- src/server.ts
+++ src/server.ts
@@ -26,63 +26,62 @@ import { createMenusRoutes } from './api/menu/routes';
 // Error Import
 import ClientError from './exceptions/ClientError';
 
-const init = async () => {
-  const app: Express = express();
-  const port = process.env.PORT || 3000;
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
-  app.use(cors());
-  app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-  // Service
-  const authenticationsService = new AuthenticationsService();
-  const usersService = new UsersService();
-  const menusService = new MenusService();
+// Service
+const authenticationsService = new AuthenticationsService();
+const usersService = new UsersService();
+const menusService = new MenusService();
 
-  // Handler
-  const authenticationsHandler = new AuthenticationsHandler(
-    authenticationsService,
-    usersService,
-    TokenManager,
-    AuthenticationsValidator
-  );
-  const usersHandler = new UsersHandler(usersService, UsersValidator);
-  const menusHandler = new MenusHandler(menusService, MenusValidator);
+// Handler
+const authenticationsHandler = new AuthenticationsHandler(
+  authenticationsService,
+  usersService,
+  TokenManager,
+  AuthenticationsValidator
+);
+const usersHandler = new UsersHandler(usersService, UsersValidator);
+const menusHandler = new MenusHandler(menusService, MenusValidator);
 
-  // Router
-  const authRouter = createAuthRoutes(authenticationsHandler);
-  const usersRouter = createUsersRoutes(usersHandler);
-  const menusRouter = createMenusRoutes(menusHandler);
+// Router
+const authRouter = createAuthRoutes(authenticationsHandler);
+const usersRouter = createUsersRoutes(usersHandler);
+const menusRouter = createMenusRoutes(menusHandler);
 
-  // Routing
-  app.use('/authentications', authRouter);
-  app.use('/users', usersRouter);
-  app.use('/menu', menusRouter);
+// Routing
+app.use('/authentications', authRouter);
+app.use('/users', usersRouter);
+app.use('/menu', menusRouter);
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof ClientError) {
-      res.status(err.statusCode).json({
-        status: 'fail',
-        message: err.message,
-      });
-      return;
-    }
-
-    // Server Error
-    console.error(err);
-    res.status(500).json({
-      status: 'error',
-      message: 'Maaf, terjadi kegagalan pada server kami.',
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ClientError) {
+    res.status(err.statusCode).json({
+      status: 'fail',
+      message: err.message,
     });
-  });
+    return;
+  }
 
+  // Server Error
+  console.error(err);
+  res.status(500).json({
+    status: 'error',
+    message: 'Maaf, terjadi kegagalan pada server kami.',
+  });
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
     const host = process.env.HOST || 'localhost';
     console.log(`Server berjalan pada http://${host}:${port}`);
   });
-};
-
-init();
+}
